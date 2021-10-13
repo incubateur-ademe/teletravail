e2fc6797a7fa966a06296851c5854818acb18414
Florian <hi@florianpanchout.com>
2021-10-13 19:28:21 +0200

    Issue 68

--- src/components/wrappers/ShareWrapper.js
+++ src/components/wrappers/ShareWrapper.js
@@ -6,8 +6,58 @@ export default function ShareWrapper(props) {
   return (
     <Share
       small={props.small}
-      title={process.env.REACT_APP_NAME}
-      message={process.env.REACT_APP_DESCRIPTION}
+      messages={{
+        mail: {
+          simulator: {
+            subject: `Découvrez et intégrez le simulateur de l'ADEME Télétravail-Mon Impact Transport !`,
+            body: `Bonjour,
+
+Vous souhaitez sensibiliser votre communauté ou collaborateurs sur les bénéfices du télétravail pour l’environnement ?
+
+Évaluez facilement les économies de CO2 réalisées grâce à au simulateur Télétravail-Mon Impact Transport !
+
+Découvrez le ici : `,
+          },
+          result: {
+            subject: `Découvre mon résultat sur le simulateur de l'ADEME Télétravail-Mon Impact Transport`,
+            body: `Bonjour,
+
+Voici un résultat de recherche sur le site Télétravail-Mon Impact Transport que je voulais partager avec toi : `,
+          },
+        },
+        facebook: {
+          simulator: {
+            quote: 'Évaluez vos économies de CO2 grâce au Télétravail 💸🌍',
+          },
+          result: {
+            quote: `Découvrez les émissions de CO2 que j’ai évitées grâce au télétravail ! `,
+          },
+        },
+        twitter: {
+          simulator: {
+            title: 'Évaluez vos économies de CO2 grâce au Télétravail 💸🌍',
+          },
+          result: {
+            title: `Découvrez les émissions de CO2 que j’ai évitées grâce au télétravail ! `,
+          },
+        },
+        linkedin: {
+          simulator: {
+            source: 'Teletravail - Mon Impact Transport',
+          },
+          result: {
+            source: `Teletravail - Mon Impact Transport`,
+          },
+        },
+        whatsapp: {
+          simulator: {
+            title: 'Évaluez vos économies de CO2 grâce au Télétravail 💸🌍',
+          },
+          result: {
+            title: `Découvrez les émissions de CO2 que j’ai évitées grâce au télétravail ! `,
+          },
+        },
+      }}
     />
   )
 }
